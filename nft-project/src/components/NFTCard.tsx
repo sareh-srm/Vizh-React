b6ebb846573c02edd8dfc53df29046b85e1aeefd
sareh-srm <sareh.82.srm@gmail.com>
2023-11-14 18:01:12 +0330

    feat - Update the main page and include NFT Cards in it

--- nft-project/src/components/NFTCard.tsx
+++ nft-project/src/components/NFTCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const NFTCard: React.FC = () => {
+type CardProps = {
+     imageUrl: string;
+}
+
+const NFTCard: React.FC<CardProps> = (props) => {
   return (
     <div className="w-64 h-96">
         <div className="rounded-t-xl bg-nftName h-1/7 p-4 shadow-md">NFT Name</div>
-        <img src="./src/assets/nftPic1.svg" alt="NFT Card" className="w-full my-1" />
+        <img src={props.imageUrl} alt="NFT Card" className="w-full my-1" />
         <div className="rounded-b-xl bg-nftName p-4 shadow-md">Artist Name
             <br />
             Current Bid 0.4 ETH
--- nft-project/src/components/NFTList.tsx
+++ nft-project/src/components/NFTList.tsx
@@ -5,15 +5,15 @@ const NFTList: React.FC = () => {
   return (
     <main className="grid grid-cols-4 gap-8 p-4">
         <NFTCard imageUrl="./src/assets/nftPic1.svg" />
-        <NFTCard imageUrl="./src/assets/nftPic2" />
-        <NFTCard imageUrl="./src/assets/nftPic3" />
-        <NFTCard imageUrl="./src/assets/nftPic4" />
-        <NFTCard imageUrl="./src/assets/nftPic5" />
-        <NFTCard imageUrl="./src/assets/nftPic6" />
-        <NFTCard imageUrl="./src/assets/nftPic7" />
-        <NFTCard imageUrl="./src/assets/nftPic8" />
-        <NFTCard imageUrl="./src/assets/nftPic9" />
-        <NFTCard imageUrl="./src/assets/nftPic10" />
+        <NFTCard imageUrl="./src/assets/nftPic2.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic3.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic4.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic5.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic6.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic7.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic8.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic9.svg" />
+        <NFTCard imageUrl="./src/assets/nftPic10.svg" />
     </main>
   )
 }
