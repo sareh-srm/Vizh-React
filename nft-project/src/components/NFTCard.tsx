a178d43de03354491f68569eb6a7e55ac3f9d342
sareh-srm <sareh.82.srm@gmail.com>
2023-11-14 19:36:16 +0330

    feat - Add Footer section

--- nft-project/src/components/NFTCard.tsx
+++ nft-project/src/components/NFTCard.tsx
@@ -8,7 +8,7 @@ const NFTCard: React.FC<CardProps> = (props) => {
   return (
     <div className="w-64 h-96">
         <div className="rounded-t-xl bg-nftName h-1/7 p-4 shadow-md">NFT Name</div>
-        <img src={props.imageUrl} alt="NFT Card" className="w-full my-1" />
+        <img src={props.imageUrl} alt="NFT Card" className="w-full my-1 object-cover" />
         <div className="rounded-b-xl bg-nftName p-4 shadow-md">Artist Name
             <br />
             Current Bid 0.4 ETH
--- nft-project/tailwind.config.js
+++ nft-project/tailwind.config.js
@@ -7,7 +7,12 @@ export default {
       white: "#FFFFFF",
       black: "#000000",
       searchbar: "#F5F5F5",
-      nftName: "#f8f8f8"
+      nftName: "#f8f8f8",
+      footer: "#0E0E0E",
+      submitButton: "#93D39B",
+      collections: "#BFBFBF",
+      aboutUs: "#F8F8F8",
+      tds: "#5B5B5B",
     },
     boxSizing: ['border-box'],
   },
