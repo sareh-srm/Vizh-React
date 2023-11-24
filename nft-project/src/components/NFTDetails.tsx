0bfe563754e09f38323831eabde56c808c93870f
sareh-srm <sareh.82.srm@gmail.com>
2023-11-24 23:31:12 +0330

    feat: [WIP] Implemented routing and updated NFT cards details page.

--- nft-project/src/components/NFTDetails.tsx
+++ nft-project/src/components/NFTDetails.tsx
@@ -1,46 +1,58 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 
 const NFTDetails: React.FC = () => {
   return (
     <div>
-      <Header />
-      <main className="flex justify-center">
-        <div className="w-3/6 h-full bg-gray rounded-2xl">
-          <h5>From "Collection Name"</h5>
-          <h6>Artwork Name</h6>
-          <div>
-            <h4>Royalties</h4>
-            <label htmlFor="">2.5%</label>
+      <main className="flex justify-between p-6 h-full">
+        <div className="w-3/6 h-full bg-gray rounded-2xl m-4 pt-10 pl-12 text-black">
+          <h2 className="text-3xl font-normal mb-10">From "Collection Name"</h2>
+          <h1 className="text-5xl font-normal mb-6">Artwork Name</h1>
+          <div className="flex w-48 justify-between mb-6">
+            <h4 className="text-2xl font-normal">Royalties</h4>
+            <span className="text-xl font-normal text-center bg-green rounded-lg w-16 h-8">
+              2.5%
+            </span>
           </div>
-          <p>
+          <p className="text-xl font-normal text-detailsText mb-10">
             You are about to buy this artwork from
             <br /> "COLLECTION NAME" collection.
           </p>
-          <div>
-            <label htmlFor="">Floor Price</label>
-            <div>
+          <div className="flex justify-between w-64 items-baseline mb-6">
+            <label className="text-base font-normal">Floor Price</label>
+            <div className="flex justify-evenly items-center w-40 h-11 rounded-2xl bg-white text-sm font-normal">
               <span>0.25</span>
-              <span>ETH</span>
+              <span className="rounded-2xl bg-green w-14 h-6 text-center">
+                ETH
+              </span>
             </div>
           </div>
-          <div>
-            <label htmlFor="">Mintii fee</label>
-            <div>2.5%</div>
+          <div className="flex justify-between w-64 items-baseline mb-6">
+            <label className="text-base font-normal">Mintii fee</label>
+            <span className="flex justify-start items-center w-40 h-11 rounded-2xl bg-white text-sm font-normal pl-6">
+              2.5%
+            </span>
           </div>
-          <div>
-            <label htmlFor="">You will pay</label>
-            <div>
+          <div className="flex justify-between w-64 items-baseline mb-6">
+            <label className="text-base font-normal">You will pay</label>
+            <div className="flex justify-evenly items-center w-40 h-11 rounded-2xl bg-white text-sm font-normal">
               <span>0.27</span>
-              <span>ETH</span>
+              <span className="rounded-2xl bg-green w-14 h-6 text-center">
+                ETH
+              </span>
             </div>
           </div>
-          <button>Submit Purchase</button>
+          <div className="flex justify-center">
+            <button className="bg-black text-aboutUs text-3xl font-normal rounded-full w-96 h-16 my-10">
+              Submit Purchase
+            </button>
+          </div>
         </div>
-        <img src="" alt="" className="w-3/6 h-full rounded-2xl" />
+        <img
+          src="./src/assets/nftPic4.svg"
+          alt=""
+          className="w-3/6 h-full object-contain rounded-2xl m-4"
+        />
       </main>
-      <Footer />
     </div>
   );
 };
