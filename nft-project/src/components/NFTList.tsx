8295d8b11deaf5e591ebe3888853d18f254aeb5e
sareh-srm <sareh.82.srm@gmail.com>
2024-07-07 12:08:41 +0330

    feat: Use map for rendering components

--- nft-project/src/components/NFTList.tsx
+++ nft-project/src/components/NFTList.tsx
@@ -1,40 +1,104 @@
 import React from "react";
 import NFTCard from "./NFTCard";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+const info = [
+  {
+    nftName: "NFT Name",
+    imageURL: "../public/assets/nftPic1.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic2.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic3.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic4.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic5.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic6.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic7.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic8.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic9.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+  {
+    nftName: "NFT Name",
+    imageURL: "./assets/nftPic10.svg",
+    logoURL: "./assets/profile-icon.svg",
+    artistName: "Artist Name",
+    currentBid: "Current Bid",
+    price: "0.4 ETH",
+  },
+];
 
 const NFTList: React.FC = () => {
   return (
     <main className="flex flex-wrap justify-start my-auto gap-10 px-4">
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic1.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic2.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic3.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic4.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic5.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic6.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic7.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic8.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic9.svg" />
-      </Link>
-      <Link to="/details">
-        <NFTCard imageUrl="./src/assets/nftPic10.svg" />
-      </Link>
+      {info.map((information) => (
+        <NFTCard
+          nftName={information.nftName}
+          imageURL={information.imageURL}
+          logoURL={information.logoURL}
+          artistName={information.artistName}
+          currentBid={information.currentBid}
+          price={information.price}
+          key={information.nftName}
+        />
+      ))}
     </main>
   );
 };
