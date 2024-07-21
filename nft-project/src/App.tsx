654930c39c33d573eccdf83a84ec7f11ce64f948
sareh-srm <sareh.82.srm@gmail.com>
2024-07-21 07:42:35 +0330

    chore:[WIP] Login routing

--- nft-project/src/App.tsx
+++ nft-project/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import NFTList from "./components/NFTList";
 import Footer from "./components/Footer";
-import NFTDetails from "./components/NFTDetails";
+//import NFTDetails from "./components/NFTDetails";
+import Login from "./components/Login";
 
 const App: React.FC = () => {
   return (
@@ -12,10 +13,12 @@ const App: React.FC = () => {
       <Routes>
         {/* this should be the landing page */}
         <Route path="/" element={<NFTList />} />
+        <Route path="/" element={<Header />} />
+        <Route path="/login" element={<Login />} />
         {/* create a login page */}
         {/* create nft list page */}
 
-        <Route path="/SubmitBid/:id" element={<NFTDetails />} />
+        <Route path="/SubmitBid/:id" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
